db7d92a81e042868691844c615cfdcb33f9d2fcf
williamhickok11 <williamhickok11@gmail.com>
2016-09-24 18:00:32 -0500

    Login and Sign Up modals are set up are ready to be filled

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -1,10 +1,61 @@
 angular.module('phonX', ['ionic'])
 
-.controller('PhonXCtrl', function($scope) {
-  $scope.tasks = [
-    { title: 'Collect coins' },
-    { title: 'Eat mushrooms' },
-    { title: 'Get high enough to grab the flag' },
-    { title: 'Find the Princess' }
-  ];
+.controller('PhonXCtrl', function($scope, $ionicModal) {
+  // No need for testing data anymore
+  $scope.tasks = [];
+
+  // Create and load the Log In Modal
+  $ionicModal.fromTemplateUrl('login.html', function(modal) {
+    $scope.loginModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+  // Create and load the Sign Up Modal
+  $ionicModal.fromTemplateUrl('signUp.html', function(modal) {
+    $scope.signUpModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.loginModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.oldUser = function() {
+    $scope.loginModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeOldUser = function() {
+    $scope.loginModal.hide();
+  };
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.signUpModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newUser = function() {
+    $scope.signUpModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewUser = function() {
+    $scope.signUpModal.hide();
+  };
+
+
 })
